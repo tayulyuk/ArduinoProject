e3cf46401efd1a246ec50bf2e375311b79032174
tayoul <yyouk123@naver.com>
2018-10-23 07:53:13 +0900

    ?

--- Esp01UnoR3MqttTest/Esp01UnoR3MqttTest.ino
+++ Esp01UnoR3MqttTest/Esp01UnoR3MqttTest.ino
@@ -9,14 +9,22 @@ IPAddress server(119,205,235,214);
 char ssid[] = "KT_GiGA_2G_76C7";           // your network SSID (name)
 char pass[] = "4jf38gf684";           // your network password
 int status = WL_IDLE_STATUS;   // the Wifi radio's status
-char* topic ="ggg";
-char* inTopic ="inggg";
+char* topic ="siheung/namu/*/";
+char* inTopic ="ggg";
 // Initialize the Ethernet client object
 WiFiEspClient espClient;
 
 PubSubClient client(espClient);
 
 SoftwareSerial soft(3,2); // RX, TX
+
+//버튼들
+char* button1 = "off";
+char* button2 = "off";
+char* button3 = "off";
+char* button4 = "off";
+char* buttonPower = "off";  
+
 void setup() {
   // initialize serial for debugging
   Serial.begin(9600);
@@ -53,12 +61,30 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
+  String str = "";
   for (int i=0;i<length;i++) {
     Serial.print((char)payload[i]);
+    str += (char)payload[i];
+  }   
+ Serial.println("str1:" + str);  
+   String tops = topic;
+   //버튼 1번 토픽이 들어왔을경우
+  if(tops == "siheung/namu/button1")
+  {
+    Serial.println("button1");
+    //각 버튼에 상태 입력하고
+    //브로커에게 다시 전송한다. 
   }
-  Serial.println();
-  
-  
+   //버튼 1번 토픽이 들어왔을경우
+  if(tops == "siheung/namu/button2")
+  {
+    Serial.println("button2");
+    //각 버튼에 상태 입력하고
+    //브로커에게 다시 전송한다. 
+  } 
+   Serial.println("str2:" + str);  
+  Serial.println("tops:" + tops);  
+  Serial.println("str3:" + str);  
 }
 
 void loop() {
@@ -76,17 +102,21 @@ void reconnect() {
     // Attempt to connect, just a name to identify the client
     if (client.connect("arduinoClient")) {
       Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish(topic,"hello world",true);  //true -> retained 옵션 설정시 마지막 메시지가 broker에 큐 형태로 있다가
+      // Once connected, publish an announcement... 
+    //  char messge[100];       
+   // sprintf(messge,"%s%s%s%s%s%s%s%s%s%s%s","|button1=",button1,"|button2=",button2,"|button3=",button3,"|button4=",button4,"|buttonPower=",buttonPower,"|");
+    //puts(messge);
+    //  client.publish(topic,messge,true);  //true -> retained 옵션 설정시 마지막 메시지가 broker에 큐 형태로 있다가
       //다른 subcribe가 접속하면 큐에있던 메시지를 보낸다.-> 마지막 상태를 알수 있다.
+      
       // ... and resubscribe
-      client.subscribe(inTopic);
+      client.subscribe(topic);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+      // Wait 5 seconds before retrying      
+      delay(5000);      
     }
   }
 }
