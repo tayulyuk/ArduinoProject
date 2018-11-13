5b650e3d7d68b0bd20df9a95c5c5cb549b4e41d5
tayoul <yyouk123@naver.com>
2018-11-13 12:16:50 +0900

    시흥 블루베리 MoterControl에 수정해서 보관함 나중에 업데이트 해드리자.

--- MoterControl/MoterControl.ino
+++ MoterControl/MoterControl.ino
@@ -1,61 +1,88 @@
-/*
-  Simple wemos D1 mini  MQTT example
-  This sketch demonstrates the capabilities of the pubsub library in combination
-  with the ESP8266 board/library.
-  It connects to the provided access point using dhcp, using ssid and pswd
-  It connects to an MQTT server ( using mqtt_server ) then:
-  - publishes "connected"+uniqueID to the [root topic] ( using topic ) 
-  - subscribes to the topic "[root topic]/composeClientID()/in"  with a callback to handle
-  - If the first character of the topic "[root topic]/composeClientID()/in" is an 1, 
-    switch ON the ESP Led, else switch it off
-  - after a delay of "[root topic]/composeClientID()/in" minimum, it will publish 
-    a composed payload to 
-  It will reconnect to the server if the connection is lost using a blocking
-  reconnect function. 
-  
-*/
-
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-// Update these with values suitable for your network.
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
 
-const char* ssid = "KT_GiGA_2G_76C7";
-const char* pswd = "4jf38gf684";
-const char* mqtt_server = "119.205.235.214";
-const char* topic = "ggg";    // rhis is the [root topic]
+// 아래의 6개설정은 사용자 환경에 맞게 수정하세요.
+//const char* ssid = "KT_GiGA_2G_76C7"; // 와이파이 AP, 또는 스마트폰의 핫스판 이름
+//const char* password = "4jf38gf684";  // 와이파이 AP, 또는 스마트폰의 핫스판 이름
 
-long timeBetweenMessages = 1000 * 20 * 1;
+const char* mqtt_server = "119.205.235.214"; //브로커 주소
+
+
+const char* outTopic = "siheung/namu/result"; // 밖으로 내보내는 토픽.
+const char* clientName = "980303Client";  // 다음 이름이 중복되지 않게 꼭 수정 바람 - 생년월일 추천
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-long lastMsg = 0;
-int value = 0;
+//long lastMsg = 0;
+//char msg[100];
+/*
+//int led=4; // D2 GPIO4 핀을 사용
+int led=BUILTIN_LED; // D1 mini에 있는 led를 사용
+int timeIn=1000;  // led가 깜박이는 시간을 mqtt 통신에서 전달받아 저장
+*/
 
-int status = WL_IDLE_STATUS;     // the starting Wifi radio's status
 
-void setup_wifi() {
-  delay(3000);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-   
-  delay(2000);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println(".");
-    Serial.println("WiFi.Status():" + String(WiFi.status()));
-    Serial.println();
-delay(500);
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  }
+//버튼 값들 저장.
+char button1[10]="0";
+char button2[10]="0";
+char button3[10]="0";
+char button4[10]="0";
+char buttonPower[10]="0";
 
+String inString="";
+String topis ="";
+
+char* mchar; //클라이언트로 부터 버튼의 값을 받는다. 이것으로 각 버튼에게 저장한다
+char *messge;// 버튼들의 값을 모아서 클라이언트로 보내기 위한 것.
+
+int buttonPin1 = 5;
+int buttonPin2 = 4;
+int buttonPin3 = 14;
+int buttonPin4 = 12;
+int buttonPinPower = 13;
+
+void setup() {
+  pinMode(buttonPin1, OUTPUT);
+  pinMode(buttonPin2, OUTPUT);
+  pinMode(buttonPin3, OUTPUT);
+  pinMode(buttonPin4, OUTPUT);
+  pinMode(buttonPinPower, OUTPUT);
+  
+  //바로 꺼준다.
+  AllSetHigh();
+    
+  Serial.begin(115200);
+  
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around   
+  WiFiManager wifiManager;
+    
+  wifiManager.autoConnect("AutoConnectAP");
+  
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  //wifiManager.startConfigPortal("OnDemandAP");
+}
+
+// high해야 꺼지고  low해야 켜짐
+void AllSetHigh()
+{
+  digitalWrite(buttonPinPower, HIGH);
+  digitalWrite(buttonPin1, HIGH);
+  digitalWrite(buttonPin2, HIGH);
+  digitalWrite(buttonPin3, HIGH);
+  digitalWrite(buttonPin4, HIGH);
+}
+
+// 통신에서 문자가 들어오면 이 함수의 payload 배열에 저장된다.
 void callback(char* topic, byte* payload, unsigned int length) {
+  inString = "";
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
@@ -64,66 +91,134 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
   Serial.println();
 
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is acive low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  // payload로 들어온 문자를 정수로 바꾸기 위해 String inString에 저장후에
+  
+ mchar = (char*)calloc((length + 1),sizeof(char));
+ messge = (char*)calloc(110,sizeof(char));
+ 
+  for (int i = 0; i < length; i++) {
+    inString += (char)payload[i];
+    mchar[i] +=(char)payload[i];
   }
+  
+  Serial.println("getString :" + inString);
+ 
+   topis = topic;
+   if(topis == "siheung/namu/button1")  
+   {
+    Serial.println("button1");
+    //각 버튼에 상태 입력하고 
+    *button1 = *mchar;  
+        
+    //<코딩과 제어에서 혼돈할수 있는 부분. >
+    //제어신호에 0을 입력시 릴레이가 ON 되며 COM공통단자와 NO단자가 연결되고 
+    //제어신호에 1을 입력시 릴레이가 OFF되며 COM공통단자와 NC단자가 연결됩니다.즉, COM은 공통단자입니다.
+    //0을 입력시 꺼지길 바란다.-> (버튼,HIGH)              1을 입력시 켜지길 바란다 ->(버튼, LOW )
+    
+    //버튼의 결과가 0이면 끔/1이면 켬
+    if(inString == "0") //끔
+    {
+     digitalWrite(buttonPin1, HIGH);
+    }
+    if(inString == "1")//켬
+    {
+      digitalWrite(buttonPin1, LOW);
+    }
+   }
+    if(topis == "siheung/namu/button2")  
+   {
+    Serial.println("button2");
+    //각 버튼에 상태 입력하고 
+    *button2 = *mchar;   
+    
+     //버튼의 결과가 0이면 끔/1이면 켬
+    if(inString == "0") //끔
+    {
+     digitalWrite(buttonPin2, HIGH);
+    }
+    if(inString == "1")//켬
+    {
+      digitalWrite(buttonPin2, LOW);
+    }
+   }
+    if(topis == "siheung/namu/button3")  
+   {
+    Serial.println("button3");
+    //각 버튼에 상태 입력하고 
+    *button3 = *mchar;   
+
+     //버튼의 결과가 0이면 끔/1이면 켬
+    if(inString == "0") //끔
+    {
+     digitalWrite(buttonPin3, HIGH);
+    }
+    if(inString == "1")//켬
+    {
+      digitalWrite(buttonPin3, LOW);
+    }
+   }
+    if(topis == "siheung/namu/button4")  
+   {
+    Serial.println("button4");
+    //각 버튼에 상태 입력하고 
+    *button4 = *mchar;   
+
+     //버튼의 결과가 0이면 끔/1이면 켬
+    if(inString == "0") //끔
+    {
+     digitalWrite(buttonPin4, HIGH);
+    }
+    if(inString == "1")//켬
+    {
+      digitalWrite(buttonPin4, LOW);
+    }
+   }
+    if(topis == "siheung/namu/buttonPower")  
+   {
+    Serial.println("buttonPower");
+    //각 버튼에 상태 입력하고 
+    *buttonPower = *mchar;   
+
+     //버튼의 결과가 0이면 끔/1이면 켬
+    if(inString == "0") //끔
+    {
+     digitalWrite(buttonPinPower, HIGH);
+    }
+    if(inString == "1")//켬
+    {
+      digitalWrite(buttonPinPower, LOW);
+    }
+   }
+   //다시 클라이언트로 현재의 버튼 상황을 보낸다. & mqtt서버에 마지막 정보를 또한 저장된다. 항상클라이언트는 마지막 정보를 받는다.
+    sprintf(messge,"|button1=%s|button2=%s|button3=%s|button4=%s|buttonPower=%s|",button1,button2,button3,button4,buttonPower);   
+    client.publish(outTopic,messge,true);  //true -> retained 옵션 설정시 마지막 메시지가 broker에 큐 형태로 있다가
+  // sprintf(messge,"%s",button1);
+   //client.publish(outTopic,messge,true); 
+    //다른 subcribe가 접속하면 큐에있던 메시지를 보낸다.-> 마지막 상태를 알수 있다.      
+    free(messge);
+    free(mchar);
 }
 
-String macToStr(const uint8_t* mac)
-{
-  String result;
-  for (int i = 0; i < 6; ++i) {
-    result += String(mac[i], 16);
-    if (i < 5)
-      result += ':';
-  }
-  return result;
-}
-
-String composeClientID() {
-  uint8_t mac[6];
- WiFi.macAddress(mac); 
-  String clientId;
-  clientId += "esp-";
-  clientId += macToStr(mac);
-  return clientId;
-}
-
+// mqtt 통신에 지속적으로 접속한다.
 void reconnect() {
   // Loop until we're reconnected
-   Serial.print("client.connected() : " + String(client.connected()));
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-
-    String clientId = composeClientID() ;
-    clientId += "-";
-    clientId += String(micros() & 0xff, 16); // to randomise. sort of
-
     // Attempt to connect
-    if (client.connect(clientId.c_str())) {
+    if (client.connect(clientName)) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish(topic, ("connected " + composeClientID()).c_str() , true );
-      // ... and resubscribe
-      // topic + clientID + in
-      String subscription;
-      subscription += topic;
-      subscription += "/";
-      subscription += composeClientID() ;
-      subscription += "/in";
-      client.subscribe(subscription.c_str() );
-      Serial.print("subscribed to : ");
-      Serial.println(subscription);
+      client.publish(outTopic, "Reconnected");
+      // ... and resubscribe    
+     client.subscribe("siheung/namu/button1");
+     client.subscribe("siheung/namu/button2");
+     client.subscribe("siheung/namu/button3");
+     client.subscribe("siheung/namu/button4");
+     client.subscribe("siheung/namu/buttonPower");
+        
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
-      Serial.print(" wifi=");
-      Serial.print(WiFi.status());
       Serial.println(" try again in 5 seconds");
       // Wait 5 seconds before retrying
       delay(5000);
@@ -131,42 +226,17 @@ void reconnect() {
   }
 }
 
-void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
-  Serial.begin(115200);
-  setup_wifi();
-  client.setServer(mqtt_server,1883);
-  delay(2000);
-  client.setCallback(callback);
-}
-
 void loop() {
-  // confirm still connected to mqtt server
+
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
-
-  long now = millis();
-  if (now - lastMsg > timeBetweenMessages ) {
-    lastMsg = now;
-    ++value;
-    String payload = "{\"micros\":";
-    payload += micros();
-    payload += ",\"counter\":";
-    payload += value;
-    payload += ",\"client\":";
-    payload += composeClientID();
-    payload += "}";
-    String pubTopic;
-     pubTopic += topic ;
-     pubTopic += "/";
-     pubTopic += composeClientID();
-     pubTopic += "/out";
-    Serial.print("Publish topic: ");
-    Serial.println(pubTopic);
-    Serial.print("Publish message: ");
-    Serial.println(payload);
-    client.publish( (char*) pubTopic.c_str() , (char*) payload.c_str(), true );
-  }
+/*
+  // 들어온 timeIn 값에 따라 led가 점멸하게 한다.
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(timeIn);                       // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(timeIn); 
+  */
 }
