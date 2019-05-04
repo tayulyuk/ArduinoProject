7023e3c507e91807b75aa21b6f53051ff6461a72
tayoul <yyouk123@naver.com>
2019-05-04 14:53:32 +0900

    Hagabi_Mqtt_Sender_WemosD1  에   OTA 와 wifiManager 추가했다.

--- Hagabi_Mqtt_Sender_WemosD1/Hagabi_Mqtt_Sender_WemosD1.ino
+++ Hagabi_Mqtt_Sender_WemosD1/Hagabi_Mqtt_Sender_WemosD1.ino
@@ -1,8 +1,13 @@
+//해가비 하우스 
 
 //통신으로 받은 값을 메가로 전송만 한다
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+
+//OTA
+#include <ESP8266mDNS.h>
+#include <ArduinoOTA.h>
 
 //needed for library
 #include <DNSServer.h>
@@ -14,12 +19,15 @@
 //const char* password = "4jf38gf684";  // 와이파이 AP, 또는 스마트폰의 핫스판 이름
 
 const char* mqtt_server = "119.205.235.214"; //브로커 주소
-// 이 주소는 GUID 활용하자//1동 -> Hagabi_1_Auto_AP_Setting   2동 -> Hagabi_2_Auto_AP_Setting
-const char* wifi_name ="Hagabi_2_Auto_AP_Setting";
+
+// 이 주소는 GUID 활용하자
+//1동 -> Hagabi_1_Auto_AP_Setting &  2동 -> Hagabi_2_Auto_AP_Setting
+const char* wifi_name ="Hagabi_1_Auto_AP_Setting";
 
 const char* outTopic = "Hagabi/result"; // 밖으로 내보내는 토픽. -> 1,2동 같은 결과로 받는다.
-// GUID를 활용하자.test1 ->1동  test2 -> 2동
-const char* subscribeName = "Hagabi/test2";
+// GUID를 활용하자.
+//test1 ->1동  test2 -> 2동
+const char* subscribeName = "Hagabi/test1";
 
 const char* clientName = "980303Client";  // 다음 이름이 중복되지 않게 꼭 수정 바람 - 생년월일 추천
 
@@ -40,10 +48,42 @@ void setup() {
   
   //WiFiManager
   //Local intialization. Once its business is done, there is no need to keep it around   
-  WiFiManager wifiManager;
-    
-  //wifiManager.autoConnect("Auto_Connect_AP");
-  wifiManager.autoConnect(wifi_name);
+  WiFiManager wifiManager;    
+  
+   if (!wifiManager.autoConnect(wifi_name)) {
+    Serial.println("failed to connect and hit timeout");
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(1000);
+  }
+  //-- OTA setup start
+    ArduinoOTA.onStart([]() {
+    Serial.println("Start OTA");
+  });
+
+  ArduinoOTA.onEnd([]() {
+    Serial.println("End OTA");
+  });
+
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\n", (progress / (total / 100)));
+  });
+
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  //----- end OTA setup
   
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
@@ -52,12 +92,7 @@ void setup() {
 
 // 통신에서 문자가 들어오면 이 함수의 payload 배열에 저장된다.
 void callback(char* topic, byte* payload, unsigned int length) {
-  /*
-  inString = "";  
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  */
+
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
@@ -68,38 +103,9 @@ void callback(char* topic, byte* payload, unsigned int length) {
 /// 다시 받은 값을 비교해서 같다면
 ////다음 작업을 해라 
 
-
-  // payload로 들어온 문자를 정수로 바꾸기 위해 String inString에 저장후에
-  /*
- mchar = (char*)calloc((length + 1),sizeof(char));
- messge = (char*)calloc(110,sizeof(char));
- 
-  for (int i = 0; i < length; i++) {
-    inString += (char)payload[i];
-    mchar[i] +=(char)payload[i];
-  }
-  
-  //Serial.println("getString :" + inString);
- Serial.println(inString);
-   topis = topic;
-   if(topis == "ModelOnOff/button1")  
-   {
-    
-   }    
-   //Serial.println("topis :" + topis);
-   */
-   /*
-   //다시 클라이언트로 현재의 버튼 상황을 보낸다. & mqtt서버에 마지막 정보를 또한 저장된다. 항상클라이언트는 마지막 정보를 받는다.
-    sprintf(messge,"|button1=%s|button2=%s|button3=%s|button4=%s|buttonPower=%s|",button1,button2,button3,button4,buttonPower);   
-    client.publish(outTopic,messge,true);  //true -> retained 옵션 설정시 마지막 메시지가 broker에 큐 형태로 있다가
-    */
-  // sprintf(messge,"%s",button1);
    //client.publish(outTopic,messge,true); 
-    //다른 subcribe가 접속하면 큐에있던 메시지를 보낸다.-> 마지막 상태를 알수 있다.     
-    /* 
-    free(messge);
-    free(mchar);
-    */
+   //다른 subcribe가 접속하면 큐에있던 메시지를 보낸다.-> 마지막 상태를 알수 있다.     
+   
 }
 
 // mqtt 통신에 지속적으로 접속한다.
@@ -115,11 +121,7 @@ void reconnect() {
       // ... and resubscribe    
 
       // GUID를 활용하자.
-     client.subscribe(subscribeName);
-     //client.subscribe("ModelOnOff/button2");
-     //client.subscribe("ModelOnOff/button3");
-     //client.subscribe("ModelOnOff/button4");
-     //client.subscribe("ModelOnOff/buttonPower");
+     client.subscribe(subscribeName);     
         
     } else {
       Serial.print("failed, rc=");
@@ -132,7 +134,16 @@ void reconnect() {
 }
 
 void loop() {
-
+ //-- 추가 와이파이 start
+  while ( WiFi.status() != WL_CONNECTED ) 
+  {
+    if (WiFi.status() == WL_CONNECTED) 
+    {
+     Serial.println("WiFi Re_connected");
+    }      
+  }
+  //--  추가 와이파이 end  
+  
   if (!client.connected()) {
     reconnect();
   }
@@ -153,19 +164,17 @@ void loop() {
       command += c;
     }
   }
+
+   //OTA
+  ArduinoOTA.handle();
 }
 
 //클라이언트에 받은 값을 돌려 보낸다.
 void SendCommand(String msg)
 {
-  char* cm = (char*)calloc((msg.length() + 1),sizeof(char));
-  //char* cm = (char*)calloc(110,sizeof(char));
-  msg.toCharArray(cm,msg.length());
- // sprintf(cm,"%s",c);   
-  //char ch[32] = {0};
-  //msg.toCharArray(ch,msg.length());
-    client.publish(outTopic, cm);
-    
-    free(cm);
+  char* cm = (char*)calloc((msg.length() + 1),sizeof(char)); 
+  msg.toCharArray(cm,msg.length()); 
+  client.publish(outTopic, cm);    
+  free(cm);
 }
 
